d9e2b8725909b35be2b156dcb02bb170a0279fa0
mehdicharfoon <mehdicharfoon@gmail.com>
2022-01-05 18:48:21 -0500

    shopping project

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>marketplace</h1>
+        <h2></h2>
+      </div>
+    );
+  }
+}
